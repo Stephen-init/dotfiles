23d1808f3df2bcd289479d9567981f9fae7d06b1
Stephen Sun <stephen@yellowcanary.com.au>
2024-04-01 09:31:06 +1100

    add some useless animation

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -107,3 +107,10 @@ vim.keymap.set("n", "[t", function()
 end, { desc = "Previous todo comment" })
 
 keybind("n", "<leader>td", ":TodoQuickFix<CR>")
+
+-- Press <C-b> to call specs!
+vim.api.nvim_set_keymap("n", "<C-b>", ':lua require("specs").show_specs()', { noremap = true, silent = true })
+
+-- You can even bind it to search jumping and more, example:
+vim.api.nvim_set_keymap("n", "n", 'n:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "N", 'N:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
