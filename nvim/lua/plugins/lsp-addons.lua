08298fad4e9c2f7a72c0eee84fe54c0ab93fc3d2
Stephen Sun <stephen@yellowcanary.com.au>
2024-04-05 21:55:55 +1100

    clean lualine + lsp diag on top

--- nvim/lua/plugins/lsp-addons.lua
+++ nvim/lua/plugins/lsp-addons.lua
@@ -60,6 +60,20 @@ return {
 		"dgagn/diagflow.nvim",
 		-- event = 'LspAttach', This is what I use personnally and it works great
 		opts = {},
+		config = function()
+			require("diagflow").setup({
+				show_sign = true, -- set to true if you want to render the diagnostic sign before the diagnostic message
+				border_chars = {
+					top_left = "┌",
+					top_right = "┐",
+					bottom_left = "└",
+					bottom_right = "┘",
+					horizontal = "",
+					vertical = "",
+				},
+				show_borders = false, -- ...
+			})
+		end,
 	},
 
 	--{
