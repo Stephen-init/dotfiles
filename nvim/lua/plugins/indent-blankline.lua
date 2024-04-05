439fda535bb75a3a5387456cf4df4aa0ec0a40a6
Stephen Sun <stephen@yellowcanary.com.au>
2024-04-05 15:28:15 +1100

    replace indent with hlchunk

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,18 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    lazy = false,
-    opts = {},
-    main = "ibl",
-    config = function()
-        require("ibl").setup()
-    end,
+	{
+		"shellRaining/hlchunk.nvim",
+		event = { "UIEnter" },
+		config = function()
+			require("hlchunk").setup({})
+		end,
+	},
+
+	--    "lukas-reineke/indent-blankline.nvim",
+	--    lazy = false,
+	--    opts = {},
+	--    main = "ibl",
+	--    config = function()
+	--        require("ibl").setup()
+	--    end,
 }
+
